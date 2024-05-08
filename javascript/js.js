6d11b37a3a41e6267d877b593ee547fe3cda6b44
coufda <121868818+coufda@users.noreply.github.com>
2024-05-08 20:24:58 +0200

    Add files via upload

--- javascript/js.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(document).foundation();
-
-    // Hack to get off-canvas .menu-icon to fire on iOS
-    $('.navbar-toggler-icon').click(function(){ false });
-});
